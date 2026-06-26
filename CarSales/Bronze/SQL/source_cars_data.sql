86c6b5e454d20e7988ea81478c7228b6d451b21f
anki7237 <98167204+anki7237@users.noreply.github.com>
2026-06-26 13:03:21 +0530

    Create source_cars_data.sql

---
+++ CarSales/Bronze/SQL/source_cars_data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE source_cars_data
+(
+    Branch_ID     VARCHAR(200)  NULL,
+    Dealer_ID     VARCHAR(200)  NULL,
+    Model_ID      VARCHAR(200)  NULL,
+    Revenue       BIGINT        NULL,
+    Units_Sold    INT           NULL,
+    Date_ID       VARCHAR(200)  NULL,
+    Day           INT           NULL,
+    Month         INT           NULL,
+    Year          INT           NULL,
+    BranchName    VARCHAR(2000) NULL,
+    DealerName    VARCHAR(2000) NULL
+);
